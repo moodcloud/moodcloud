55dd876d0e25ce5cd141d63fea5ed465868f16fd
Eivenlour David <eivenlou@ualberta.ca>
2019-01-12 11:42:09 -0700

    Adding react app framework

---
+++ build/precache-manifest.fbdbfe414009b89b9b09896fb0814af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a226e69d1955e508c527",
+    "url": "/static/css/main.31c3957a.chunk.css"
+  },
+  {
+    "revision": "a226e69d1955e508c527",
+    "url": "/static/js/main.a226e69d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "926fc6007991bc2c2262680c3bc568d3",
+    "url": "/index.html"
+  }
+];
